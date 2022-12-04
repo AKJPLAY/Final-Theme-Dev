a131c46781ed16fcf1f18cbc16f13c14fae57108
AKJPLAY <38575828+AKJPLAY@users.noreply.github.com>
2022-12-04 21:41:59 +0530

    Update theme-editor.js

--- assets/theme-editor.js
+++ assets/theme-editor.js
@@ -1,4 +1,12 @@
 
 document.addEventListener('shopify:section:load', (event) => {
-    console.log('edito js', event.target);
+    console.log('edito js', event.target, event.target.querySelector('.swiper'));
+    if(event.target.querySelector('.swiper')){
+        const slider = event.target.querySelector('.swiper');
+        const config = event.target.slider.getAttribute('config');
+        console.log(JSON.parse(config));
+        if(slider){
+            new Swiper('.' + this.slider.id , JSON.parse(this.config));
+        }    
+    }
 });
