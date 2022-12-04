41f4f87fd8c81aee6c8d5511cc1924fe0001a846
AKJPLAY <38575828+AKJPLAY@users.noreply.github.com>
2022-12-04 21:45:52 +0530

    Update theme-editor.js

--- assets/theme-editor.js
+++ assets/theme-editor.js
@@ -3,7 +3,7 @@ document.addEventListener('shopify:section:load', (event) => {
     console.log('edito js', event.target, event.target.querySelector('.swiper'));
     if(event.target.querySelector('.swiper')){
         const slider = event.target.querySelector('.swiper');
-        const config = event.target.slider.getAttribute('config');
+        const config = slider.getAttribute('config');
         console.log(JSON.parse(config));
         if(slider){
             new Swiper('.' + slider.id , JSON.parse(config));
